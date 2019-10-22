627fd2786f4b5876688b10c072b95a3582e23aa2
Tzila <14856070+Tzila@users.noreply.github.com>
2019-10-21 19:57:35 -0700

    Update code_challenge.py

--- 4 - String variables/code_challenge.py
+++ 4 - String variables/code_challenge.py
@@ -1,6 +1,10 @@
 # ask a user to enter their first name and store it in a variable
+firstName = input('first name ')
 # ask a user to enter their last name and store it in a variable
+lastName = input('last name')
 # print their full name
 # Make sure you have a space between first and last name
+system.print firstName + ' ' + lastName
 # Make sure the first letter of first name and last name is uppercase
+system.print firstName(capitalize) + " " + lastName(capitalize)
 # Make sure the rest of the name is lowercase
