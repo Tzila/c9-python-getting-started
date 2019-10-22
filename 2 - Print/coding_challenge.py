e810c69eb1c67ec146add0699100b59684a6aa15
Tzila <wdelaney421@gmail.com>
2019-10-21 17:41:39 -0700

    Update coding_challenge.py

--- 2 - Print/coding_challenge.py
+++ 2 - Print/coding_challenge.py
@@ -1,15 +1,17 @@
 # Here's a challenge for you to help you practice
-# See if you can fix the code below 
+# See if you can fix the code below
 
 # print the message
-print('Why won't this line of code print')
+print ("Why won't this line of code print")
 
 # print the message
-prnit('This line fails too!')
+print ("This line fails too!")
 
 # print the message
-print "I think I know how to fix this one"
+print ("I think I know how to fix this one")
+
+print ('test 12,\n test 12')
 
 # print the name entered by the user
-input('Please tell me your name: ')
+name = input('Please tell me your name: ')
 print(name)
