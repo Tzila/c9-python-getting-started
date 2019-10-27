b042b7aba3372d9a00a5d50021fcd7b5a2530d9d
Tzila <14856070+Tzila@users.noreply.github.com>
2019-10-26 19:16:40 -0700

    Daily update

--- 10 - Complex conditon checks/code_challenge.py
+++ 10 - Complex conditon checks/code_challenge.py
@@ -1,18 +1,25 @@
 # When you join a hockey team you get your name on the back of the jersey
 # but the jersey may not be big enough to hold all the letters
 # Ask the user for their first name
-
+userFirstName = input('What is your first name? ')
 # Ask the user for their last name
-
-# if first name is < 10 characters and last name is < 10 characters 
-#       print first and last name on the jersey
+userLastName = input('What is your last name? ')
+# if first name is < 10 characters and last name is < 10 characters
+# print first and last name on the jersey
+if len(userFirstName) < 10 and len(userLastName) < 10:
+    print(userFirstName.capitalize() + ' ' + userLastName.capitalize())
 # if first name >= 10 characters long and last name is < 10 characters
 #       print first initial of first name and the entire last name
+elif len(userFirstName) >= 10 and len(userLastName) < 10:
+    print((userFirstName[0].capitalize()) + ' ' + userLastName.capitalize())
 # if first name < 10 characters long and last name is >= 10 characters
 #       print entire first name and first initial of last name
+elif len(userFirstName) < 10 and len(userLastName) >= 10:
+    print(userFirstName.capitalize() + ' ' + userLastName[0].capitalize())
 # if first name >= 10 characters long and last name is >= 10 characters
 #       print last name only
-
+elif len(userFirstName) >= 10 and len(userLastName) >= 10:
+    print(userLastName.capitalize())
 # Test with the following values
 # first name: Susan  last name: Ibach
 # output: Susan Ibach
